c8844d509eabb53a7ce2d65de7f3d932319d37cc
szadoq <kweniek@gmail.com>
2021-02-10 21:33:37 +0100

    host mem stats

--- vir_switch_server/jobs.py
+++ vir_switch_server/jobs.py
@@ -1,16 +1,70 @@
-import pickle
-import socket as sock
-import json
 import subprocess
 from subprocess import PIPE
-from datetime import  datetime
+from datetime import datetime
+import sqlite3
+import hashlib
+
+c = sqlite3.connect('vir_switch_server/users.db')
+cursor = c.cursor()
 
 
-def read_users_file():
-    with open('vir_switch_server/users.json') as users_file:
-        users = json.load(users_file)
-        print(type(users))
-        return users
+def create_table():
+    init_pass = hashlib.md5(b'admin').hexdigest()
+    query = f"CREATE TABLE IF NOT EXISTS users(login TEXT UNIQUE , password TEXT , admin TEXT , vms TEXT );"
+    cursor.execute(query)
+    query2 = f"SELECT login FROM users WHERE admin = 'yes';"
+    cursor.execute(query2)
+    admins = cursor.fetchall()
+    c.commit()
+    if len(admins) == 0:
+        query2 = f"INSERT INTO users(login, password, admin, vms) VALUES ('admin','{init_pass}', 'yes', 'all');"
+        cursor.execute(query2)
+        c.commit()
+
+
+def add_user(username, user_data):
+    pass_hash = user_data[0]
+    is_admin = user_data[1]
+    vms = user_data[2]
+
+    query = "INSERT INTO users(login, password, admin, vms) VALUES (?, ?, ?, ?);"
+    cursor.execute(query, (username, pass_hash, is_admin, vms))
+    c.commit()
+
+
+def delete_user(username):
+    query = f"DELETE FROM users WHERE login ='{username}';"
+    cursor.execute(query)
+    c.commit()
+
+
+def check_user(username, password):
+    query = f"SELECT login, password, admin, vms FROM users WHERE login = '{username}';"
+    cursor.execute(query)
+    check = cursor.fetchone()
+    c.commit()
+    if check:
+        if password in check:
+            user = check[0]
+            is_admin = check[2]
+            vms = check[3]
+            msg = ['password_ok', user, 'pass', is_admin, vms]
+            add_logs_entry(user, action=f'login to sever')
+            return msg
+        else:
+            msg = ['password_wrong']
+            return msg
+    else:
+        msg = ['password_wrong']
+        return msg
+
+
+def users_list():
+    query = f"SELECT login, admin, vms FROM users ORDER BY admin DESC ;"
+    cursor.execute(query)
+    u_list = cursor.fetchall()
+    c.commit()
+    return u_list
 
 
 def read_logs_file():
@@ -20,35 +74,35 @@ def read_logs_file():
     return logs
 
 
-def add_logs_entry(user, address, action):
+def add_logs_entry(user, action):
     f = open('vir_switch_server/logs.txt', 'a+', encoding='utf-8')
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y - %H:%M:%S")
-    f.write(f'{dt_string} ----- FROM: {address[0]} ----- USER: {user} ----- ACTION: {action}\n')
+    f.write(f'{dt_string} ----- USER: {user} ----- ACTION: {action}\n')
     f.close()
 
 
-def reset_logs():
-    return
+def reset_logs(user):
+    f = open('vir_switch_server/logs.txt', 'w', encoding='utf-8')
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y - %H:%M:%S")
+    f.write(f'{dt_string} --- CLEAR LOGS BY USER: {user}\n')
+    f.close()
 
 
 def control_vm(cmd):
-    out = ''
-    err = ''
     try:
         p = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE, shell=True)
         stdout, stderr = p.communicate()
-
         out_raw = stdout.decode('utf-8')
         err = stderr.decode('utf-8')
-        out = []
     except Exception as er:
         err = str(er)
         print(err)
-        out = out_raw.split('\n')
-    for line in out:
-        print(type(line), line)
-    return
+    out = out_raw.split()
+    print(out_raw)
+    print(out)
+    return out
 
 
 def make_vm_list(cmd):
@@ -57,23 +111,40 @@ def make_vm_list(cmd):
     try:
         p = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE, shell=True)
         stdout, stderr = p.communicate()
-
         out = stdout.decode('utf-8')
         err = stderr.decode('utf-8')
         v_list = []
+        v_list_info = list()
     except Exception as er:
         err = str(er)
     out_raw = out.split('\n')
     for line in out_raw[2:-2]:
         li = line.split()
-        li.pop(0)
-        if len(li) > 2:
-            li[1] = li[1] + ' ' + li[2]
-            li.pop(2)
-        print(li)
-        print(line)
-        v_list.append(li)
-    print(v_list)
+        # print(li)
+        v_list_info.append(li[1])
+
+    for vm in v_list_info:
+        try:
+            p = subprocess.Popen(f"echo 'gugugu' | sudo -S virsh dominfo {vm} | grep '\(State\|memory\|CPU\)'", stdout=PIPE, stderr=PIPE, stdin=PIPE, shell=True)
+            stdout, stderr = p.communicate()
+
+            out = stdout.decode('utf-8')
+            err = stderr.decode('utf-8')
+            info = []
+        except Exception as er:
+            err = str(er)
+        out_raw = out[0:-2].split('\n')
+        info.append(vm)
+        state = out_raw[0].split()[-1]
+        info.append(state)
+        cpu = out_raw[1].split()[-1]
+        info.append(cpu)
+        max_memory = int(int(out_raw[-2].split()[-2]) / 1024)
+        info.append(max_memory)
+        memory = int(int(out_raw[-1].split()[-2]) / 1024)
+        info.append(memory)
+        # print(info)
+        v_list.append(info)
 
     return v_list
 
--- vir_switch_server/main.py
+++ vir_switch_server/main.py
@@ -4,7 +4,8 @@ import jobs
 from time import sleep
 
 HOST = '192.168.0.64'
-PORT = 4444
+PORT = 3333
+jobs.create_table()
 
 if __name__ == '__main__':
     print("--- start server ---")
@@ -13,46 +14,71 @@ server_socket = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(3)
 
-
 while True:
     client_socket, address = server_socket.accept()
-    data_from = client_socket.recv(50000)
-
-    users = jobs.read_users_file()
+    data_from = client_socket.recv(32767)
 
     if data_from:
         print("otrzymany pakiet binarny: ", data_from)
-        log_and_pas = pickle.loads(data_from)
-        print(log_and_pas, type(log_and_pas))
-        msg_id = log_and_pas[0]
-        data2 = log_and_pas[1]
-        data3 = log_and_pas[2]
+        data_pack = pickle.loads(data_from)
+        print(data_pack, type(data_pack))
+        msg_id = data_pack[0]
+        a_user = data_pack[1]
+        data2 = data_pack[2]
+        data3 = data_pack[3]
 
-        print(f"Uzyskano połączenie od hosta {address[0]} na porcie: {address[1]} wiadomość: {log_and_pas}")
+        print(f"Uzyskano połączenie od hosta {address[0]} na porcie: {address[1]} wiadomość: {data_pack}")
 
         if msg_id == "user_check":
-            jobs.add_logs_entry(data2, address, action='login')
-            if data2 in list(users.keys()) and data3 == users[data2][0]:
-                pack_msg = pickle.dumps(['password_ok', data2, users[data2][0], users[data2][1], users[data2][2]])
-            else:
-                pack_msg = pickle.dumps(['password_wrong'])
+            print(jobs.check_user(a_user, data3))
+            print(data3)
+            pack_msg = pickle.dumps(jobs.check_user(a_user, data3))
 
         elif msg_id == "get_user_list":
+            pack_msg = pickle.dumps(jobs.users_list())
 
-            pack_msg = pickle.dumps(users)
+        elif msg_id == "add_user":
+            jobs.add_user(a_user, data2)
+            pack_msg = pickle.dumps(jobs.users_list())
+
+        elif msg_id == "delete_user":
+            jobs.delete_user(a_user)
+            pack_msg = pickle.dumps(jobs.users_list())
+
+        elif msg_id == "host_memory":
+            host = jobs.control_vm(f'free -m| grep Mem')
+            print(host)
+            pack_msg = pickle.dumps(host)
 
         elif msg_id == "v_list":
-
             v_list = jobs.make_vm_list('echo "gugugu" | sudo -S virsh list --all ')
             pack_msg = pickle.dumps(v_list)
 
         elif msg_id == "get_logs":
             logs = jobs.read_logs_file()
-            print(type(logs), logs)
             pack_msg = pickle.dumps(logs)
+
+        elif msg_id == "reset_logs":
+            logs = jobs.reset_logs(a_user)
+            pack_msg = pickle.dumps(logs)
+
+        elif msg_id == "new_memory":
+            print(data2, data3)
+            jobs.control_vm(f'echo "gugugu" | sudo -S virsh setmem {data2} {data3}M')
+            jobs.add_logs_entry(a_user, action=f'set memory {data2}: {data3} MB')
+            v_list = jobs.make_vm_list('echo "gugugu" | sudo -S virsh list --all ')
+            pack_msg = pickle.dumps(v_list)
+
+        elif msg_id == "new_max_memory":
+            print(data2, data3)
+            jobs.control_vm(f'echo "gugugu" | sudo -S virsh setmaxmem {data2} {data3}M')
+            jobs.add_logs_entry(a_user, action=f'set maxmemory {data2}: {data3} MB')
+            v_list = jobs.make_vm_list('echo "gugugu" | sudo -S virsh list --all ')
+            pack_msg = pickle.dumps(v_list)
 
         elif msg_id == "start":
             jobs.control_vm(f'echo "gugugu" | sudo -S virsh start {data2} ')
+            jobs.add_logs_entry(a_user, action=f'start {data2}')
             s = '--- komenda start dla '
             print(msg_id, data2, s, data2)
             sleep(1)
@@ -61,6 +87,7 @@ while True:
 
         elif msg_id == "stop":
             jobs.control_vm(f'echo "gugugu" | sudo -S virsh shutdown {data2} ')
+            jobs.add_logs_entry(a_user, action=f'stop {data2}')
             s = '--- komenda shutdown dla '
             print(msg_id, data2, s, data2)
             sleep(1)
@@ -69,6 +96,7 @@ while True:
 
         elif msg_id == "restart":
             jobs.control_vm(f'echo "gugugu" | sudo -S virsh reboot {data2} ')
+            jobs.add_logs_entry(a_user, action=f'reboot {data2}')
             s = '--- komenda reboot dla'
             print(msg_id, data2, s, data2)
             sleep(1)
@@ -77,17 +105,16 @@ while True:
 
         elif msg_id == "kill":
             jobs.control_vm(f'echo "gugugu" | sudo -S virsh destroy {data2} ')
+            jobs.add_logs_entry(a_user, action=f'force stop {data2}')
             s = '--- komenda destroy dla'
             print(msg_id, data2, s, data2)
-            sleep(1)
+
             v_list = jobs.make_vm_list('echo "gugugu" | sudo -S virsh list --all ')
             pack_msg = pickle.dumps(v_list)
 
         else:
             pack_msg = pickle.dumps(['password_wrong'])
-
         client_socket.send(pack_msg)
-
     else:
         print("<<< otrzymano pusty pakiet >>>")
 
